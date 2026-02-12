74424495c6817d677e4782d32ec4baf0135f5e6d
Ravikant Tyagi <150049400+sqb-codes@users.noreply.github.com>
2026-02-12 12:18:28 +0530

    Add files via upload

---
+++ Theory/rag-app/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import ragRoutes from "./routes/ragRoutes";
+
+dotenv.config()
+
+const app = express();
+app.use(express.json());
+
+app.use("/api", ragRoutes);
+
+app.listen(3545, () => {
+    console.log("Server started at PORT:3545");
+})
